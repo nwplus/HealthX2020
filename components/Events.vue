52d6b90e5e241fdf44ff4226c21ffefe49196968
Alice Kim <aliceykim0828@gmail.com>
2020-07-22 11:18:25 -0700

    Implement Events section w/o animation

--- components/Events.vue
+++ components/Events.vue
@@ -1,25 +1,30 @@
 <template>
-  <div class="mainContainer ">
+  <div class="mainContainer">
     <div class="headerText">
       Main Values
     </div>
     <div class="subText">
-      cmd-f was established according to our 3 main values:
+      HealthX is a chance for students interested in life sciences and technology to collaborate and tackle problems in the medicine and biology field.
+      <br>It was established according to our 2 main values:
     </div>
     <div class="no-overflow columns is-3-tablet">
       <div v-for="item in sortedEvents" :key="item.order">
-        <div
-          class="events column"
-        >
+        <div :class="{flipped: item.flipped}" class="events column">
           <div class="image-container">
-            <img :src="item.imageLink" :class="{smaller: isWomenSupportingWomen(item)}" class="image">
+            <img
+              :src="item.imageLink"
+              :class="{smaller: isWomenSupportingWomen(item)}"
+              class="image"
+            >
           </div>
-          <h3 class="title">
-            {{ item.title }}
-          </h3>
-          <p class="valuesText">
-            {{ item.text }}
-          </p>
+          <div class="main-container">
+            <h3 class="title">
+              {{ item.title }}
+            </h3>
+            <p class="text">
+              {{ item.text }}
+            </p>
+          </div>
         </div>
       </div>
     </div>
@@ -27,7 +32,7 @@
 </template>
 
 <script>
-import orderBy from 'lodash.orderby'
+// import orderBy from 'lodash.orderby'
 export default {
   props: {
     items: {
@@ -37,7 +42,22 @@ export default {
   },
   computed: {
     sortedEvents: function () {
-      return orderBy(this.items, 'order')
+      // return orderBy(this.items, 'order')
+      return [
+        {
+          flipped: true,
+          title: 'Collaboration Between Streams',
+          imageLink: require('@/assets/highfive.svg'),
+          text:
+            "Are you an expert in neuroscience but have never touched a line of code? Perhaps you can spin up a website, but you've never heard of a mitochondria (THE POWERHOUSE OF THE CELL)? Working in diverse teams is one of the best ways to come up with unique solutions, and we encourage teams to have a blend of both technical and life science skills."
+        },
+        {
+          title: 'Exploring New Limits',
+          imageLink: require('@/assets/universe.svg'),
+          text:
+            'HealthX is all about pushing the boundaries of what is known and unknown in the fields of medicine and life sciences. If you have an idea that you think could revolutionize the way medical research is done, or a solution that could benefit those with illnesses or disabilities, then this is the hackathon for you.'
+        }
+      ]
     }
   },
   methods: {
@@ -49,5 +69,73 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$muted-blue: #193aa0;
+$neon-blue: #78fef7;
+$light-blue: #e4eeff;
+$white: #ffffff;
 
+.mainContainer {
+  background-color: $muted-blue;
+  padding: 0 175px;
+}
+.headerText {
+  color: $neon-blue;
+  font-family: Inter;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 42px;
+  line-height: 28px;
+  text-align: center;
+  letter-spacing: 0.04em;
+}
+.subText {
+  color: $white;
+  font-family: Inter;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 35px;
+  text-align: center;
+  width: 67%;
+  margin: 35px auto;
+}
+.columns {
+  display: block;
+}
+.column {
+  display: flex;
+  margin: 70px auto;
+  &.flipped {
+    flex-direction: row-reverse;
+    img {
+      margin: 0;
+      margin-left: auto;
+    }
+  }
+  > div {
+    flex: 1;
+  }
+  .title {
+    color: $neon-blue;
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 30px;
+    line-height: 28px;
+  }
+  .text {
+    color: $light-blue;
+    font-family: Inter;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 35px;
+  }
+  .image-container img {
+    width: 388px;
+    height: 263.82px;
+    display: block;
+    margin-right: auto;
+  }
+}
 </style>
